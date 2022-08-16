fd65ad6b36e53e34145e8b4d37c3ffb366930b1d
Benen Kim <benen.k18@gmail.com>
2022-08-16 13:40:25 -0700

    created card items and shopping cart base

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,70 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query';
 
-function App() {
+//Components
+import Item from './Item/Item'
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+//Styles
+import { Wrapper, StyledButton } from './App.styles';
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  estimatedCost: number;
+  title: string;
+  amount: number;
+};
+
+const itemId = 9266;
+
+//A promise is called a generic in typescript.
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+  console.log(data);
+
+    const getTotalItems = (items: CartItemType[]) =>
+      items.reduce((ack: number, item) => ack + item.amount, 0);
+
+    const handleAddToCart = (clickedItem: CartItemType) => null;
+
+    const handleRemoveFromCart = () => null;
+
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong ...</div>
+
   return (
-    <div className="App">
-    </div>
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
