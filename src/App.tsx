488578405e3b7a166a43ea62ca13c81d5b023422
Benen Kim <benen.k18@gmail.com>
2022-08-16 14:10:16 -0700

    added addtocart function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import { useQuery } from 'react-query';
 
 //Components
-import Item from './Item/Item'
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid';
@@ -16,7 +17,7 @@ export type CartItemType = {
   category: string;
   description: string;
   image: string;
-  estimatedCost: number;
+  price: number;
   title: string;
   amount: number;
 };
@@ -37,20 +38,40 @@ const App = () => {
   );
   console.log(data);
 
-    const getTotalItems = (items: CartItemType[]) =>
-      items.reduce((ack: number, item) => ack + item.amount, 0);
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0);
 
-    const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      //1. is the item alr in cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id) //check if item exists in previous state
 
-    const handleRemoveFromCart = () => null;
+      if (isItemInCart) {
+        //can update the amount by mapping through the items.
+        return prev.map(item => (
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1} //if item found
+            : item 
+        ))
+      }
+      //First time item is added
+      return [...prev, { ...clickedItem, amount: 1}];
+    })
+  };
 
-    if (isLoading) return <LinearProgress />;
-    if (error) return <div>Something went wrong ...</div>
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong ...</div>
 
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart 
+          cartItems={cartItems} 
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+           />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color='error'>
